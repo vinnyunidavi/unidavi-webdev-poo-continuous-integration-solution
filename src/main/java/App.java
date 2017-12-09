e99967938fe15ce20e7e1c27d503e770587f7d31
Vinny <vinicius.nunes@unidavi.edu.br>
2017-12-09 09:19:40 -0200

    Resolver issues relatadas pelo sonar

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,7 @@
+import java.util.logging.Logger;
+
 public class App {
   public static void main(String[] args){
-    System.out.println("Hello");
+	  Logger.getGlobal().info("Hello");
   }
 }
--- src/main/java/solution/WordProblemSolver.java
+++ src/main/java/solution/WordProblemSolver.java
@@ -3,11 +3,12 @@ package solution;
 import java.util.HashMap;
 import java.util.Map;
 
+//XXX renomear classe
 public class WordProblemSolver {
 
 	private Map<String,Integer> results = new HashMap<String,Integer>();
 
-	{
+	WordProblemSolver(){
 		results.put("What is 53 plus 2?", 55);
 		results.put("What is 1 plus 1?", 2);
 		results.put("What is -1 plus -10?", -11);
